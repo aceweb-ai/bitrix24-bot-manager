8313535afd6e0602ecbc495f14d149a5c3fc838f
Rushing_to_freedom <orlov.aceweb@gmail.com>
2026-03-27 17:49:43 +0300

    Add optional _app.js file

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// Опциональный файл _app.js для кастомизации страниц Next.js
+// Можно использовать для глобальных стилей, провайдеров состояния и т.д.
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
